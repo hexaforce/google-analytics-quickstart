2b01bf59b8288bdc70e49d02f7a1c18d9352f7f9
tantaka <tomokazu.tantaka@glpgs.com>
2023-02-18 14:44:24 +0900

    kk

--- gunicorn-flask/app.py
+++ gunicorn-flask/app.py
@@ -13,14 +13,20 @@ SQLALCHEMY_DATABASE_URI='mysql+pymysql://{}:{}@{}/{}'.format(
     os.getenv('DB_NAME', 'analytics')
 )
 
+# create the app
 app = Flask(__name__)
+
+# configure the MariaDB database
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# initialize the app with the Database extension
 db.init_app(app)
 
+# initialize the app with the Resource API extension
 api.init_app(app)
 
+# register_blueprint
 app.register_blueprint(analytics_api)
 app.register_blueprint(analytics_data_api)
 app.register_blueprint(analytics_admin_api)

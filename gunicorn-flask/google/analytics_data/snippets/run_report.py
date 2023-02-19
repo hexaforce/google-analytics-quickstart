f4e5eadb1106fc29bb62bd8da775b4f8b75c24f5
tantaka <tomokazu.tantaka@glpgs.com>
2023-02-19 11:05:47 +0900

    update

--- gunicorn-flask/google/analytics_data/snippets/run_report.py
+++ gunicorn-flask/google/analytics_data/snippets/run_report.py
@@ -35,11 +35,11 @@ def run_sample():
     """Runs the sample."""
     # TODO(developer): Replace this variable with your Google Analytics 4
     #  property ID before running the sample.
-    property_id = "354244385"
+    property_id = "354149023"
     run_report(property_id)
 
 
-def run_report(property_id="354244385"):
+def run_report(property_id):
     """Runs a report of active users grouped by country."""
     client = BetaAnalyticsDataClient()
 

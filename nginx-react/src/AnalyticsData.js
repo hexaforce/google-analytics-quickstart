602d81b2c59c07a07b045426cf31d19f4d84a2ad
tantaka <tomokazu.tantaka@glpgs.com>
2023-03-03 14:26:41 +0900

    up

--- nginx-react/src/AnalyticsData.js
+++ nginx-react/src/AnalyticsData.js
@@ -5,7 +5,7 @@ import { GET, POST } from './HttpClient'
 import Metric from './AnalyticsDataMetric'
 import Dimension from './AnalyticsDataDimension'
 
-import { Button, Stack, Dropdown, Form, ListGroup, OverlayTrigger, Tooltip, Container, Row, Col } from 'react-bootstrap';
+import { Button, Stack, Card, Form, ListGroup, OverlayTrigger, Tooltip, Container, Row, Col, Modal } from 'react-bootstrap';
 import React, { useState, useRef } from 'react';
 
 import './AnalyticsData.css'
@@ -42,52 +42,81 @@ const AnalyticsData = () => {
     setEndDate(e.target.value)
   }
 
+  const [show, setShow] = useState(false);
+  const [result, setResult] = useState();
+  const [error, setError] = useState();
+
   const runReport = async (e) => {
+    setError(undefined)
     const response = await POST(`/api/data/run-report/${property_id}`, { dimension_name: dimension_name, metric_name: metric_name, start_date: start_date, end_date: end_date })
     if (response.ok) {
       const body = await response.json();
       console.log(body)
+      setResult(body)
+      setShow(true)
+    } else {
+      setError({
+        url: response.url,
+        status: response.status,
+        statusText: response.statusText,
+      })
     }
   }
 
   return (
-    <Form className='m-3' >
-      <Form.Group className="m-3">
-        <Form.Label>PropertyId</Form.Label>
-        <Form.Control type="property_id" placeholder="Property ID" onChange={onPropertyId} />
-      </Form.Group>
-      <div className="Container">
-        <div className="OverflowY">
-          <Form.Label>Dimension</Form.Label>
-          <ListGroup onClick={onDimension}>
-            {Dimension.map((e, i) => <OverlayTrigger key={i} overlay={<Tooltip >{e.説明}</Tooltip>}>
-              <ListGroup.Item active={dimension_name === e.name} action variant="light">{e.UI名}</ListGroup.Item>
-            </OverlayTrigger>
-            )}
-          </ListGroup>
-        </div>
-        <div className="OverflowY">
-          <Form.Label>Metric</Form.Label>
-          <ListGroup onClick={onMetric}>
-            {Metric.map((e, i) => <OverlayTrigger key={i} overlay={<Tooltip>{e.説明}</Tooltip>}>
-              <ListGroup.Item active={metric_name === e.name} action variant="light">{e.UI名}</ListGroup.Item>
-            </OverlayTrigger>
-            )}
-          </ListGroup>
-        </div>
-        <Form.Group className="m-3">
-          <Form.Label>StartDate</Form.Label>
-          <Form.Control defaultValue="5daysAgo" type="start_date" placeholder="`YYYY-MM-DD` or `(N)daysAgo` or `yesterday` or `today`" onChange={onStartDate} />
-        </Form.Group>
-        <Form.Group className="m-3">
-          <Form.Label>EndDate</Form.Label>
-          <Form.Control defaultValue="today" type="end_date" placeholder="`YYYY-MM-DD` or `(N)daysAgo` or `yesterday` or `today`" onChange={onEndDate} />
-        </Form.Group>
-      </div>
-      <Button variant="primary" onClick={runReport}>
-        Submit
+    <Card>
+      <Card.Body>
+        <Card.Title>AnalyticsData</Card.Title>
+        <pre style={{ color: "red" }}>{JSON.stringify(error, undefined, 2)}</pre>
+        <Form className='m-3' >
+          <Form.Group className="m-3">
+            <Form.Label>PropertyId</Form.Label>
+            <Form.Control type="property_id" placeholder="Property ID" onChange={onPropertyId} />
+          </Form.Group>
+          <div className="Container">
+            <div className="OverflowY">
+              <Form.Label>Dimension</Form.Label>
+              <ListGroup onClick={onDimension}>
+                {Dimension.map((e, i) => <OverlayTrigger key={i} overlay={<Tooltip >{e.説明}</Tooltip>}>
+                  <ListGroup.Item active={dimension_name === e.name} action variant="light">{e.UI名}</ListGroup.Item>
+                </OverlayTrigger>
+                )}
+              </ListGroup>
+            </div>
+            <div className="OverflowY">
+              <Form.Label>Metric</Form.Label>
+              <ListGroup onClick={onMetric}>
+                {Metric.map((e, i) => <OverlayTrigger key={i} overlay={<Tooltip>{e.説明}</Tooltip>}>
+                  <ListGroup.Item active={metric_name === e.name} action variant="light">{e.UI名}</ListGroup.Item>
+                </OverlayTrigger>
+                )}
+              </ListGroup>
+            </div>
+            <Form.Group className="m-3">
+              <Form.Label>StartDate</Form.Label>
+              <Form.Control defaultValue="5daysAgo" type="start_date" placeholder="`YYYY-MM-DD` or `(N)daysAgo` or `yesterday` or `today`" onChange={onStartDate} />
+            </Form.Group>
+            <Form.Group className="m-3">
+              <Form.Label>EndDate</Form.Label>
+              <Form.Control defaultValue="today" type="end_date" placeholder="`YYYY-MM-DD` or `(N)daysAgo` or `yesterday` or `today`" onChange={onEndDate} />
+            </Form.Group>
+          </div>
+        </Form>
+      </Card.Body>
+
+      <Button className='mx-5 px-5' variant="primary" onClick={runReport}>
+        Run Report
       </Button>
-    </Form>
+
+      <Modal show={show} fullscreen onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Result</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <pre>{JSON.stringify(result, undefined, 2)}</pre>
+        </Modal.Body>
+      </Modal>
+    </Card>
   );
 }
 
